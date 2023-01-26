b10adc60073caf580f5f7c0e4ab056d912162333
rpc28970 <rpc28970@gmail.com>
2023-01-26 16:44:46 -0500

    Adding and Removing items from cart functionality

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -13,30 +13,73 @@ import {
   Link,
   HStack,
   Text,
+  useToast,
 } from "@chakra-ui/react";
 import { FiShoppingCart } from "react-icons/fi";
 import { Link as ReactLink } from "react-router-dom";
 import { StarIcon } from "@chakra-ui/icons";
+import { useDispatch, useSelector } from "react-redux";
+import { addCartItem } from "../redux/actions/cartActions";
 
-const Rating = ({rating, numberOfReviews}) => {
-    const {iconSize, setIconSize} = useState('14px');
-    return (
-        <Flex>
-            <HStack spacing='2px' >
-                <StarIcon size={iconSize} w='14px' color='orange.500' />
-                <StarIcon size={iconSize} w='14px' color={rating >= 2 ? 'orange.500' : 'gray.200'} />
-                <StarIcon size={iconSize} w='14px' color={rating >= 3 ? 'orange.500' : 'gray.200'} />
-                <StarIcon size={iconSize} w='14px' color={rating >= 4 ? 'orange.500' : 'gray.200'} />
-                <StarIcon size={iconSize} w='14px' color={rating >= 5 ? 'orange.500' : 'gray.200'} />
-            </HStack>
-            <Text fontSize='md' fontWeight='bold' ml='4px' >
-                {`${numberOfReviews} ${numberOfReviews === 1 ? 'Review' : 'Reviews'}`}
-            </Text>
-        </Flex>
-    )
-}
+const Rating = ({ rating, numberOfReviews }) => {
+  const { iconSize, setIconSize } = useState("14px");
+  return (
+    <Flex>
+      <HStack spacing="2px">
+        <StarIcon size={iconSize} w="14px" color="orange.500" />
+        <StarIcon
+          size={iconSize}
+          w="14px"
+          color={rating >= 2 ? "orange.500" : "gray.200"}
+        />
+        <StarIcon
+          size={iconSize}
+          w="14px"
+          color={rating >= 3 ? "orange.500" : "gray.200"}
+        />
+        <StarIcon
+          size={iconSize}
+          w="14px"
+          color={rating >= 4 ? "orange.500" : "gray.200"}
+        />
+        <StarIcon
+          size={iconSize}
+          w="14px"
+          color={rating >= 5 ? "orange.500" : "gray.200"}
+        />
+      </HStack>
+      <Text fontSize="md" fontWeight="bold" ml="4px">
+        {`${numberOfReviews} ${numberOfReviews === 1 ? "Review" : "Reviews"}`}
+      </Text>
+    </Flex>
+  );
+};
 
 const ProductCard = ({ product }) => {
+  const dispatch = useDispatch();
+  const toast = useToast();
+
+  const cartInfo = useSelector((state) => state.cart);
+  const { cart } = cartInfo;
+
+  const addItem = (id) => {
+    if (cart.some((cartItem) => cartItem.id === id)) {
+      toast({
+        description:
+          "This item is already in your cart. Go to your cart to change the amount.",
+        status: "error",
+        isClosable: true,
+      });
+    } else {
+      dispatch(addCartItem(id, 1));
+      toast({
+        description: "Item has been added",
+        status: "success",
+        isClosable: true,
+      });
+    }
+  };
+
   return (
     <Stack
       p="2"
@@ -92,8 +135,11 @@ const ProductCard = ({ product }) => {
           </Box>
         </Link>
       </Flex>
-      <Flex justifyContent='space-between' alignContent='center' py='2' >
-        <Rating rating={product.rating} numberOfReviews={product.numberOfReviews} />
+      <Flex justifyContent="space-between" alignContent="center" py="2">
+        <Rating
+          rating={product.rating}
+          numberOfReviews={product.numberOfReviews}
+        />
       </Flex>
       <Flex justify="space-between">
         <Box fontSize="2xl" color={useColorModeValue("gray.800", "white")}>
@@ -109,7 +155,12 @@ const ProductCard = ({ product }) => {
           color="gray.800"
           fontSize="1.2em"
         >
-          <Button variant="ghost" display="flex" isDisabled={product.stock <= 0}>
+          <Button
+            variant="ghost"
+            display="flex"
+            isDisabled={product.stock <= 0}
+            onClick={() => addItem(product._id)}
+          >
             <Icon as={FiShoppingCart} h={7} w={7} alignSelf="center" />
           </Button>
         </Tooltip>
